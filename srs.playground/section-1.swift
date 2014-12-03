b5b93c43516d238f152be6663b7864bd0c4bab05
Matthew Kanwisher <matt@kanwisher.com>
2014-12-04 00:19:02 +0700

    cleanup initial functions

--- srs.playground/section-1.swift
+++ srs.playground/section-1.swift
@@ -8,69 +8,12 @@ let QUALITY_SUBTRACTOR = 5
 let E_FACTOR_FLOOR:Float = 1.3
 
 struct Card {
-    var eFactor: Float = 0.0
+    var eFactor: Float = 2.5
     var id: String?
     var count: Int = 0
     var interval: Int = 0
-
-    // identifier in the database
-    public int id;
-    // the cards title
-    public String title;
-    // urls pointing to the files the card has
-    public URL side1;
-    public URL side2;
-    // the current e-factor for this card
-    public float eFactor = 2.5f;
-    public int count = 0;
-    public Date lastTime;
-    public int interval;
-    
-    public Card(
-    int id, String title, URL side1, URL side2, float eFactor) {
-    this.id = id;
-    this.title = title;
-    this.side1 = side1;
-    this.side2 = side2;
-    this.eFactor = eFactor;
-    }
-    
-    public Card(String title, URL side1, URL side2) {
-    this.title = title;
-    this.side1 = side1;
-    this.side2 = side2;
-    this.eFactor = 0;
-    }
-    
-    public int getId() {
-    return id;
-    }
-    
-    public float getEFactor() {
-    return eFactor;
-    }
-    
-    public void setEFactor(float factor) {
-    eFactor = factor;
-    }
-    
-    public int getCount() {
-    return count;
-    }
-    
-    public void setCount(int count) {
-    this.count = count;
-    }
-    
-    public float getInterval() {
-    return interval;
-    }
-    
-    public void setInterval(int interval) {
-    this.interval = interval;
-    }
-
-
+    var lastTime: NSDate
+//    public String title;
 }
 
 //Score a Card and set it's correct E-Factor.
@@ -88,11 +31,11 @@ func calcuateInterval(inout card:Card) {
         card.count = 1
     }
     let count = card.count
-    var interval:Float = 1
+    var interval = 1
     if (count == 2) {
         interval = 6
     } else if (count > 2) {
-        interval =  round(card.interval * card.eFactor);
+        card.interval =  Int(round(Float(card.interval) * card.eFactor))
     }
     card.interval = interval
 }
